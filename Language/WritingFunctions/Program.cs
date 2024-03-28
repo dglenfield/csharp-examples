1036823623d628041e6603cabe8e0ad917614ab7
Danny Glenfield <dglenfield@gmail.com>
2024-03-28 10:21:17 -0400

    Add function examples

---
+++ Language/WritingFunctions/Program.cs
@@ -0,0 +1,28 @@
+﻿
+TimesTable(7);
+TimesTable(number: 7, size: 20);
+
+// Writing a function that returns a value.
+ConfigureConsole(culture: "fr-FR");
+decimal taxToPay = CalculateTax(149, "FR");
+WriteLine($"You must pay {taxToPay:C} in tax.");
+
+ConfigureConsole();
+
+WriteLine();
+
+// Converting numbers from cardinal to ordinal.
+RunCardinalToOrdinal();
+
+WriteLine();
+
+// Calculating factorials with recursion.
+RunFactorial();
+
+WriteLine();
+
+// Using lambdas in function implementations.
+RunFibImperative();
+WriteLine();
+RunFibFunctional();
+
