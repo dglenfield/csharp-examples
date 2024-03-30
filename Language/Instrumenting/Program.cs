9b75d5bfc1c6b9cd6adc2f3b027d3a2b0aa0a85d
Danny Glenfield <dglenfield@gmail.com>
2024-03-30 11:57:04 -0400

    Add instrumenting (logging) examples

---
+++ Language/Instrumenting/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+using System.Diagnostics; // To use Debug and Trace.
+
+// Writing to the default trace listener.
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
+
+// Configuring trace listeners.
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+WriteLine($"Writing to: {logPath}");
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+/* Any type that represents a file usually implements a buffer to improve performance. Instead of writing
+ * immediately to the file, data is written to an in-memory buffer, and only once the buffer is full will
+ * it be written in one chunk to the file. This behavior can be confusing while debugging because we do
+ * not immediately see the results. 
+ * 
+ * Enabling AutoFlush means the Flush method is called automatically after every write. This reduces 
+ * performance, so you should only set it on during debugging and not in production. */
+#if DEBUG
+// Text writer is buffered, so this option calls Flush() on all listeners after writing.
+Trace.AutoFlush = true;
+#endif
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
+
+// Close the text file (also flushes) and release resources.
+Debug.Close();
+Trace.Close();
+
+/* When running with the Debug configuration, both Debug and Trace are active and will write to any trace listeners.
+ * When running with the Release configuration, only Trace will write to any trace listeners. 
+ * You can therefore use Debug.WriteLine calls liberally throughout your code, knowing they will be stripped out 
+ * automatically when you build the release version of your application and will not affect performance. */
---
+++ Language/Instrumenting2/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration; // To use ConfigurationBuilder.
+using System.Diagnostics; // To use Debug and Trace.
+
+// Configuring trace listeners.
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+WriteLine($"Writing to: {logPath}");
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+Trace.Listeners.Add(logFile);
+
+#if DEBUG
+Trace.AutoFlush = true;
+#endif
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
+
+// Create a configuration builder that looks in the current folder for a file named appsettings.json 
+string settingsFile = "appsettings.json";
+string settingsPath = Path.Combine(Directory.GetCurrentDirectory(), settingsFile);
+
+WriteLine("Processing: {0}", settingsPath);
+WriteLine("--{0} contents--", settingsFile);
+WriteLine(File.ReadAllText(settingsPath));
+WriteLine("----");
+
+ConfigurationBuilder builder = new();
+
+// Add the settings file to the processed configuration and make it
+// mandatory so an exception will be thrown if the file is not found.
+builder.AddJsonFile(settingsFile, optional: false, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new("PacktSwitch", "This switch is set via a JSON config.");
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+
+WriteLine($"Trace switch value: {ts.Value}");
+WriteLine($"Trace switch level: {ts.Level}");
+
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
+int unitsInStock = 12;
+LogSourceDetails(unitsInStock > 10);
+
+// Close the text file (also flushes) and release resources.
+Debug.Close();
+Trace.Close();
+
+WriteLine("Press Enter to exit.");
+ReadLine();
