3d3e4d5d6f72598a68308a42be90a883ea47f373
Danny Glenfield <dglenfield@gmail.com>
2024-05-29 19:54:27 -0400

    Add packaging and distributing examples

---
+++ PackagingAndDistributing/AssembliesAndNamespace/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq; // TO use XDocument.
+using Packt.Shared;
+
+XDocument doc = new();
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine();
+WriteLine("Is {0} a valid color value? {1}", hex, hex.IsValidHex());
+
+Write("Enter a XML element: ");
+string? xmlTag = ReadLine();
+WriteLine("Is {0} a valid XML element? {1}", xmlTag, xmlTag.IsValidXmlTag());
+
+Write("Enter a password: ");
+string? password = ReadLine();
+WriteLine("Is {0} a valid password? {1}", password, password.IsValidHex());
+
