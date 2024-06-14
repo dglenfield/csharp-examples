ac4859c12f98fea64ecb1f903337ec385616e7df
Danny Glenfield <dglenfield@gmail.com>
2024-06-14 08:59:53 -0400

    Add examples for Common Types

---
+++ CommonTypes/WorkingWithRanges/Program.cs
@@ -0,0 +1,18 @@
+﻿string name = "Samantha Jones";
+
+// Getting the length of the first and last names.
+int lengthOfFirst = name.IndexOf(' ');
+int lengthOfLast = name.Length - lengthOfFirst - 1;
+
+// Using Substring
+string firstName = name.Substring(0, lengthOfFirst);
+string lastName = name.Substring(name.Length - lengthOfLast, lengthOfLast);
+
+WriteLine($"First: {firstName}, Last: {lastName}");
+
+// Using spans.
+ReadOnlySpan<char> nameAsSpan = name.AsSpan();
+ReadOnlySpan<char> firstNameAsSpan = nameAsSpan[0..lengthOfFirst];
+ReadOnlySpan<char> lastNameAsSpan = nameAsSpan[^lengthOfLast..];
+
+WriteLine($"First: {firstNameAsSpan}, Last: {lastNameAsSpan}");
---
+++ CommonTypes/WorkingWithRegularExpressions/Program.Regexs.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions; // TO use [GeneratedRegex].
+
+partial class Program
+{
+    [GeneratedRegex(DigitsOnlyText, RegexOptions.IgnoreCase)]
+    private static partial Regex DigitsOnly();
+
+    [GeneratedRegex(CommaSeparatorText, RegexOptions.IgnoreCase)]
+    private static partial Regex CommaSeparator();
+
+}
