4185e0b3c1f048cd13f3961835ad429418f5c7db
Danny Glenfield <dglenfield@gmail.com>
2024-06-20 11:05:58 -0400

    Add Blazor app example

---
+++ PracticalApps/Northwind.Blazor.Services/INorthwindService.cs
@@ -0,0 +1,13 @@
+﻿using Northwind.EntityModels; // To use Customer.
+
+namespace Northwind.Blazor.Services;
+
+public interface INorthwindService
+{
+    Task<Customer> CreateCustomerASync(Customer c);
+    Task DeleteCustomerASync(string id);
+    Task<Customer?> GetCustomerASync(string id);
+    Task<List<Customer>> GetCustomersASync();
+    Task<List<Customer>> GetCustomersASync(string country);
+    Task<Customer> UpdateCustomerASync(Customer c);
+}
---
+++ PracticalApps/Northwind.Blazor/Services/NorthwindServiceServerSide.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore; // To use ToListAsync<T>.
+
+namespace Northwind.Blazor.Services;
+
+public class NorthwindServiceServerSide : INorthwindService
+{
+    private readonly NorthwindContext _db;
+
+    public NorthwindServiceServerSide(NorthwindContext db)
+    {
+        _db = db;
+    }
+
+    public Task<Customer> CreateCustomerASync(Customer c)
+    {
+        _db.Customers.Add(c);
+        _db.SaveChangesAsync();
+        return Task.FromResult(c);
+    }
+
+    public Task DeleteCustomerASync(string id)
+    {
+        Customer? customer = _db.Customers.FirstOrDefaultAsync(c => c.CustomerId == id).Result;
+
+        if (customer == null)
+        {
+            return Task.CompletedTask;
+        }
+
+        _db.Customers.Remove(customer);
+        return _db.SaveChangesAsync();
+    }
+
+    public Task<Customer?> GetCustomerASync(string id)
+    {
+        return _db.Customers.FirstOrDefaultAsync(c => c.CustomerId == id);
+    }
+
+    public Task<List<Customer>> GetCustomersASync()
+    {
+        return _db.Customers.ToListAsync();
+    }
+
+    public Task<List<Customer>> GetCustomersASync(string country)
+    {
+        return _db.Customers.Where(c => c.Country == country).ToListAsync();
+    }
+
+    public Task<Customer> UpdateCustomerASync(Customer c)
+    {
+        _db.Entry(c).State = EntityState.Modified;
+        _db.SaveChangesAsync();
+        return Task.FromResult(c);
+    }
+}
