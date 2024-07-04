187fd5fb1daf282143981d0465577a452a53fce5
Danny Glenfield <dglenfield@gmail.com>
2024-07-04 12:00:07 -0400

    Add Humanizer examples

---
+++ ThirdPartyLibraries/HumanizingData/Program.cs
@@ -0,0 +1,14 @@
+﻿ConfigureConsole();
+//ConfigureConsole("fr-FR");
+
+//OutputCasings("The cat sat on the mat.");
+//OutputCasings("THE CAT SAT ON THE MAT.");
+//OutputCasings("the cat sat on the mat. the frog jumped.");
+
+//OutputSpacingAndDashes();
+
+//OutputEnumNames();
+
+//NumberFormatting();
+
+DateTimeFormatting();
